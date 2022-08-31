6363763603593905ace4a30b621ce3731b1a7564
tigerbroko <tigerbroko@gmail.com>
2022-08-31 18:25:04 +0100

    another model_search.py update 2

--- model_search.py
+++ model_search.py
@@ -17,7 +17,7 @@ def search_model(user_input):
     The function returns "soup_list" - the list of html codes for each adv. tab that we process with function 
     n_days_search.
     """
-    import re
+    import re as re
 
     user_search_input = str(user_input)
     user_search_input = re.sub(r"[^\w\s]", '', user_search_input)
