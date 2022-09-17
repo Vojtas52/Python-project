d78ba2c857e0b8600eb046e495ae9af6d25862c8
tigerbroko <tigerbroko@gmail.com>
2022-09-17 12:56:47 +0100

    removed index.html, transformation to index.j2 + inserting table and another button, bugs unsolved

--- __main__.py
+++ __main__.py
@@ -1,43 +1,34 @@
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+app.jinja_env.filters['zip'] = zip
 
-@app.route("/", methods=['GET', 'POST'])
-#@app.route("/")
-#@app.before_first_request
-def _run_on_start():
+@app.route("/")
 
-   return "Hello, I am your bazos.cz car searching engine. \n Please insert the name of your desired car model below."
-
-#@app.route("/")
 def root():
+    
     input = request.args.get('input')
     if not input:
         return render_template("index.j2")
 
+    order = []
+    date = []
+    price = []
+    mileage = []
+    yom = []
+    url = []
+
     page_data = {
-        'x-values': ["2022-8-11","2022-8-12","2022-8-13"],
-        'y-values': [250000, 275000, 265000],
+        'order-values': ["1","2","3"], #order,
+        'date-values': ["2022-8-11","2022-8-12","2022-8-13"], #date,
+        'price-values': [250000, 275000, 265000], #price,
+        'mileage-values': ["mileage 1", "mileage 2", "mileage 3"], #mileage,
+        'yom-values': ["1","2","3"], #yom,
+        'url-values': ["1","2","3"], #url,
         'input': input
     }
 
     return render_template("index.j2", page_data=page_data)
-
-#ap@app.route("/", methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        if request.form.get('action1') == 'VALUE1':
-            pass # do something
-
-        elif  request.form.get('action2') == 'VALUE2':
-            pass # do something else
-        else:
-            pass # unknown
-    elif request.method == 'GET':
-        return render_template('index.html')#, form=form)
-    
-    return render_template("index.html")
-
 
 if __name__ == "__main__":
     app.run(port=8080)
