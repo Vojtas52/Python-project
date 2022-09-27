507fa3150a6cad04878e854302870fd463526502
tigerbroko <tigerbroko@gmail.com>
2022-09-27 15:31:15 +0100

    Tried to add the dict backup in cache.py, not implemented yet... v3

--- cache.py
+++ cache.py
@@ -2,3 +2,11 @@ from requests import Response
 
 # key=url, value=response[200]
 cache: dict[str, Response] = {}
+
+# key = url, value = dictionary
+data_backup: dict[str, dict] = {}
+
+data_backup[url]['url' : url]
+data_backup[url]['date' : date] # not definied yet
+data_backup[url]['mileage' : mileage]
+data_backup[url]['yom' : year of manufacture]
